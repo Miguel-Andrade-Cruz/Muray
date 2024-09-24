dd45d489f00d501742ddd94a6f6bc090187aeedd
Angelium-Eddra <angelium.eddra1972@gmail.com>
2024-09-24 09:53:06 -0300

    delete index file

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/vendor/autoload.php';
-
-use Minuz\Muray\SquaredArray\SquaredArray;
-
-$example = new SquaredArray(3, 2, ['A1', 'A2', 'A3','A4', 'A5']);
-
-var_dump($example->acessColumn(0));
